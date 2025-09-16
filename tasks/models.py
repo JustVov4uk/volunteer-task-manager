7aaae2df7b5ce2c701ae94e4516ef1c350fffb6e
JustVov4uk <volodabudzan4@gmail.com>
2025-09-16 13:20:07 +0200

    create User model & register AUTH_USER_MODEL in settihgs

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.conf import settings
 
@@ -8,4 +9,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("koordinator", "Koordinator"),
+        ("volunteer", "Volunteer"),
+    )
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
+    phone_number = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
 
