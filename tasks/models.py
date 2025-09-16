2866c5dca2f80c141b2cae6b3430e660ad9b5d79
JustVov4uk <volodabudzan4@gmail.com>
2025-09-16 12:28:55 +0200

    add model Category

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
