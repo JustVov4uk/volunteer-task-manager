8d1c5c7f404a272d8a392139f18af0b4604a7b80
JustVov4uk <volodabudzan4@gmail.com>
2025-09-17 15:37:50 +0200

    install bootstrap and create index.html & base.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from tasks.models import CustomUser, Task, Category
 
 
-def test_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world!")
+def index(request: HttpRequest) -> HttpResponse:
+    num_volunteers = CustomUser.objects.filter(is_volunteer=True).count()
+    num_tasks = Task.objects.count()
+    num_categories = Category.objects.count()
+    context = {
+        "num_volunteers": num_volunteers,
+        "num_tasks": num_tasks,
+        "num_categories": num_categories,
+    }
+    return render(request, "tasks/index.html", context=context)
