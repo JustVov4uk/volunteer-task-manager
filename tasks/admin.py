9546b967a06a3f8fff512474da7278fe6fccbccd
JustVov4uk <volodabudzan4@gmail.com>
2025-09-17 12:17:22 +0200

    register models in admin.py & add to models list_display

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from tasks.models import (Category,
+                          CustomUser,
+                          Tag, Task,
+                          Report)
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = (UserAdmin.list_display +
+                    ("role", "phone_number",
+                     "city"))
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "description",
+        "created_by",
+        "assigned_to",
+        "status",
+        "deadline",
+        "category",
+    ]
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = [
+        "author",
+        "comment",
+        "task",
+        "verified_by",
+        "verified_at"
+    ]
