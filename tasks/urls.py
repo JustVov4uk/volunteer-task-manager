b3f12a9dbcc03a6d3213fad00e294cc34e8d629f
JustVov4uk <volodabudzan4@gmail.com>
2025-09-18 14:00:12 +0200

    create index_coordinator & index_volunteer and write view functions for redirect

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+
+from tasks import views
 from tasks.views import index, CategoryListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("category/", CategoryListView.as_view(), name="category-list"),
+    path("coordinator/", views.coordinator_index, name="coordinator-index"),
+    path("volunteer/", views.volunteer_index, name="volunteer-index"),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,43 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
-from tasks.models import CustomUser, Task, Category
+from tasks.models import CustomUser, Task, Category, Tag, Report
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    num_volunteers = CustomUser.objects.count()
+    if request.user.role =="coordinator":
+        return redirect("tasks:coordinator-index")
+    return redirect("tasks:volunteer-index")
+
+def coordinator_index(request: HttpRequest) -> HttpResponse:
+    num_volunteers = CustomUser.objects.filter(role="volunteer").count()
     num_tasks = Task.objects.count()
     num_categories = Category.objects.count()
+    num_tags = Tag.objects.count()
+    num_reports = Report.objects.count()
+
     context = {
         "num_volunteers": num_volunteers,
         "num_tasks": num_tasks,
         "num_categories": num_categories,
+        "num_tags": num_tags,
+        "num_reports": num_reports,
     }
-    return render(request, "tasks/index.html", context=context)
+    return render(request, "tasks/index_coordinator.html", context=context)
 
+
+def volunteer_index(request: HttpRequest) -> HttpResponse:
+    num_tasks = Task.objects.count()
+    num_categories = Category.objects.count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_categories": num_categories,
+    }
+
+    return render(request, "tasks/index_volunteer.html", context=context)
 
 class CategoryListView(LoginRequiredMixin, generic.ListView):
     model = Category
