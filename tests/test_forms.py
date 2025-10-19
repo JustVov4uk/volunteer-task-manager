143b7dbfb61168356fb8cf35f70d472d7962789e
JustVov4uk <volodabudzan4@gmail.com>
2025-10-19 16:16:55 +0200

    create tests for CategorySearchForm

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from tasks.forms import CategoryForm
+from tasks.forms import CategoryForm, CategorySearchForm
 
 
 class CategoryFormTest(TestCase):
@@ -21,3 +21,26 @@ class CategoryFormTest(TestCase):
         form = CategoryForm(data=form_data)
         self.assertFalse(form.is_valid())
         self.assertIn("name", form.errors)
+
+
+class CategorySearchFormTest(TestCase):
+    def test_form_valid_with_name_field(self):
+        form_data = {
+            "name": "test name",
+        }
+        form = CategorySearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["name"], "test name")
+
+    def test_form_valid_when_name_is_empty(self):
+        form_data = {
+            "name": "",
+            "description": "test description"
+        }
+        form = CategorySearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["name"], "")
+
+    def test_form_field_has_placeholder(self):
+        placeholder = CategorySearchForm().fields["name"].widget.attrs["placeholder"]
+        self.assertEqual(placeholder, "Search by name")
