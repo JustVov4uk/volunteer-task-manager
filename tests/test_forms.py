e4087aed7b84ed91f56bba45ba3ff7f96f1a8871
JustVov4uk <volodabudzan4@gmail.com>
2025-10-19 17:24:38 +0200

    create tests for CustomUserCreateForm

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from tasks.forms import CategoryForm, CategorySearchForm
+from tasks.forms import CategoryForm, CategorySearchForm, CustomUserCreateForm
 
 
 class CategoryFormTest(TestCase):
@@ -44,3 +44,37 @@ class CategorySearchFormTest(TestCase):
     def test_form_field_has_placeholder(self):
         placeholder = CategorySearchForm().fields["name"].widget.attrs["placeholder"]
         self.assertEqual(placeholder, "Search by name")
+
+
+class CustomUserCreateFormTest(TestCase):
+    def test_form_valid_with_correct_data(self):
+        form_data = {
+            "username": "user",
+            "password1": "test password",
+            "password2": "test password",
+            "first_name": "test first",
+            "last_name": "test last",
+            "email": "test@email.com",
+            "role": "volunteer",
+            "phone_number": "123456789",
+            "city": "test city",
+        }
+        form = CustomUserCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_if_username_is_empty(self):
+        form_data = {
+            "username": "",
+        }
+        form = CustomUserCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("username", form.errors)
+
+    def test_form_if_form_has_all_fields(self):
+        fields = [
+            "username", "role", "first_name", "last_name",
+            "email", "phone_number", "city", "profile_image",
+            "password1", "password2",
+        ]
+        form = CustomUserCreateForm()
+        self.assertEqual(list(form.fields.keys()), fields)
