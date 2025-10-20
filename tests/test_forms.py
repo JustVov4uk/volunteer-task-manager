b13bca6d44e6cebed94c5418d5dd3b383df8cbf3
JustVov4uk <volodabudzan4@gmail.com>
2025-10-20 17:27:08 +0200

    create tests for CustomUserUpdateForm & CustomUserSearchForm

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-from tasks.forms import CategoryForm, CategorySearchForm, CustomUserCreateForm
+from tasks.forms import (CategoryForm,
+                         CategorySearchForm,
+                         CustomUserCreateForm, CustomUserUpdateForm, CustomUserSearchForm)
 
 
 class CategoryFormTest(TestCase):
@@ -78,3 +82,70 @@ class CustomUserCreateFormTest(TestCase):
         ]
         form = CustomUserCreateForm()
         self.assertEqual(list(form.fields.keys()), fields)
+
+
+class CustomUserUpdateFormTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="old_user",
+            first_name="old",
+            last_name="user",
+            email="old@gmail.com",
+            role="volunteer",
+            phone_number="123456789",
+            city="old city",
+        )
+    def test_form_update_exist_volunteer(self):
+        form_data = {
+            "username": "new_user",
+            "first_name": "new",
+            "last_name": "user",
+            "email": "new@gmail.com",
+            "role": "volunteer",
+            "phone_number": "987654321",
+            "city": "new city",
+        }
+        form = CustomUserUpdateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        updated_user = form.save()
+        self.assertEqual(updated_user.username, "new_user")
+        self.assertEqual(updated_user.first_name, "new")
+        self.assertEqual(updated_user.last_name, "user")
+        self.assertEqual(updated_user.email, "new@gmail.com")
+        self.assertEqual(updated_user.phone_number, "987654321")
+        self.assertEqual(updated_user.city, "new city")
+
+    def test_form_valid_when_update_a_few_fields(self):
+        form_data = {
+            "username": "old_user",
+            "first_name": "old",
+            "last_name": "user",
+            "email": "old@gmail.com",
+            "role": "volunteer",
+            "phone_number": "987654321",
+            "city": "new city",
+        }
+        form = CustomUserUpdateForm(data=form_data, instance=self.user)
+        self.assertTrue(form.is_valid())
+
+
+class CustomUserSearchFormTest(TestCase):
+    def test_form_valid_with_input_username(self):
+        form_data = {
+            "username": "test_user",
+        }
+        form = CustomUserSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["username"], "test_user")
+
+    def test_form_valid_when_input_username_is_empty(self):
+        form_data = {
+            "username": "",
+        }
+        form = CustomUserSearchForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data["username"], "")
+
+    def test_form_with_placeholder(self):
+        placeholder = CustomUserSearchForm().fields["username"].widget.attrs["placeholder"]
+        self.assertEqual(placeholder, "Search by username")
