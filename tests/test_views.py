dfdd16813fa8620eea389f2d7d71727593c97de9
JustVov4uk <volodabudzan4@gmail.com>
2025-10-27 18:45:52 +0100

    create tests for CategoryDetailView

--- tests/test_views.py
+++ tests/test_views.py
@@ -328,3 +328,34 @@ class CategoryListViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertIn("/login", response.url)
 
+
+class CategoryDetailViewTest(TestCase):
+    def setUp(self):
+        self.coordinator = get_user_model().objects.create_user(
+            username="coordinator",
+            password="test password",
+            role="coordinator",
+        )
+        self.volunteer = get_user_model().objects.create_user(
+            username="volunteer",
+            password="other test password",
+            role="volunteer",
+        )
+        self.category1 = Category.objects.create(
+            name="test category1",
+            description="test description",
+        )
+        self.category2 = Category.objects.create(
+            name="test category2",
+            description="test description",
+        )
+
+    def test_view_logged_user_200(self):
+        self.client.login(username="coordinator", password="test password")
+        response = self.client.get(reverse("tasks:category-detail", args=[self.category1.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_redirect_for_anonymous(self):
+        response = self.client.get(reverse("tasks:category-detail", args=[self.category1.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/login", response.url)
