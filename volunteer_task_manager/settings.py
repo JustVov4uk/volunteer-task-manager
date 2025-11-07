c2ad22fb6f1d3f46c79122a47a8bf504ab0a5f6a
JustVov4uk <volodabudzan4@gmail.com>
2025-11-07 11:54:11 +0100

    relocate SECRET_KEY from settings.py to .env

--- volunteer_task_manager/settings.py
+++ volunteer_task_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f@c2r&tapuxkvx7=elu*#97g#z!^mb$u&=vz4s2#16%7@+^qj1"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
